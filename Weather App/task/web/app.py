d407dcc836966cb051d950790c99c44d19fd9b93
fvn70 <fvn_reg@mail.ru>
2022-10-01 12:46:18 +0700

    Stage 3/5: What's the weather today

--- Weather App/task/web/app.py
+++ Weather App/task/web/app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from random import randint, shuffle
+
+from flask import Flask, render_template, request
 import sys
 
 app = Flask(__name__)
+dict = {'name': ['night', 'day', 'evening-morning'],
+        'degrees': [9, 32, -15],
+        'state': ['Chilly', 'Sunny', 'Cold'],
+        'city': ['Boston', 'New York', 'Edmonton'] }
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        city_name = request.form['city_name']
+        dict['name'].append('autumn')
+        dict['degrees'].append(randint(-30, 30))
+        shuffle(dict['state'])
+        dict['state'].append(dict['state'][0])
+        dict['city'].append(city_name)
+    num = len(dict['name'])
+    return render_template('index.html', weather=dict, card_num=num)
 
 # don't change the following way to run flask:
 if __name__ == '__main__':
